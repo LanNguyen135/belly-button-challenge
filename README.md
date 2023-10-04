a706b927d9e0dcd0b6b0f0e024d5962f259a58b1
LanNguyen135 <lannguyen13595@gmail.com>
2023-10-03 22:47:36 -0500

    finished coding

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ initial commit
 
 Credit:
 https://github.com/dmcneill0711/Digging-Into-The-Belly-Button-Biome/tree/main
+https://plotly.com/javascript/gauge-charts/
+https://loading.io/color/feature/YlGnBu-8/
--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,9 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 function init() {
-    d3.json(url).then(function(data) {
+    d3.json(url).then(function(data) {        
+        // Add test subject id to drop down menu
         let name = data.names;
-        // Add names to drop down menu
         let dropdownMenu = d3.select("#selDataset");
         name.forEach(function(name) {
             dropdownMenu.append("option").text(name);
@@ -21,50 +21,61 @@ function init() {
         // Create default bubble chart
         bubbleChart(otuIDs, sampleValues, otuLables);
 
-        //Create default metadata table
+        // Create default metadata table
         d3.select("#sample-metadata").html(metadataTable(metadata));
 
+        // Create default gauge chart
+        let wfreq = data.metadata[0].wfreq;
+        gaugeChart(wfreq);       
     })
 };
 
-init();
-
-// Function called by DOM changes
 function optionChanged(value) {
     d3.json(url).then(function(data){
 
         let index = data.names.indexOf(String(value));
-        console.log(index);
+        // console.log(index);
         
         // Get data for the new selected person
         let dataValue = Object.values(data.samples[index].sample_values);
         let dataLabel = Object.values(data.samples[index].otu_ids);
         let dataText = Object.values(data.samples[index].otu_labels);
         let metadata = data.metadata[index];
+        let wfreq = data.metadata[index].wfreq;
 
         // Update the chart
         barChart(dataValue, dataLabel, dataText);
         bubbleChart(dataLabel, dataValue, dataText);
         d3.select("#sample-metadata").html(metadataTable(metadata))
+        gaugeChart(wfreq);
+        console.log(wfreq);
     });
 
   }
 
 function barChart(value, label, text) {
+    // Prepare the data for the bar plot
     let x = value.slice(0,10).reverse();
     let y = label.slice(0,10).map(id => `OTU ${id}`).reverse();
     let text1 = text.slice(0,10).reverse();
-
+    
     let plotData = [{
         x: x,
         y: y,
         text: text1,
         type: "bar",
-        orientation: "h"
+        orientation: "h",
+        marker: {color: 'rgb(49,130,189)',
+                opacity: 0.75}
     }]
-
+    // Update layout
+    let layout = {
+        width: 400,
+        height: 450,
+        margin: {t:0, r:0, b:25}
+    };
     // Call function to update the chart
-    Plotly.newPlot("bar", plotData);
+    Plotly.newPlot("bar", plotData, layout);
 }
 
 function bubbleChart(otuIDs, sampleValues, otuLables) {
@@ -76,20 +87,65 @@ function bubbleChart(otuIDs, sampleValues, otuLables) {
         mode: "markers",
         marker: {
             size: sampleValues,
-            color: otuIDs
+            color: otuIDs,
+            colorscale: 'YlGnBu'
         }
     }]
 
-    Plotly.newPlot("bubble", bubblePlot);
+    let layout = {
+        width: 1200,
+        height: 400,
+        margin: {t: 25, r: 25, l: 25, b: 25}
+    }
+    Plotly.newPlot("bubble", bubblePlot, layout);
 };
 
 function metadataTable(metadata) {
     let rowValue = [];
     for (var key in metadata) {
         let value = metadata[key]
-        rowValue += `${key}: ${value}<br><br>`;
+        rowValue += `${key}: ${value}<br>`;
     }
     // console.log(keys, values);
     // console.log(metadata);
     return rowValue;
 };
+
+function gaugeChart(number) {
+    let washData = [{
+            type: "indicator",            
+            mode: "gauge+number",           
+            value: number,           
+            title: {text: `<b>Belly Button Washing Frequency</b> <br>Scrubs per Week`},                   
+            gauge: {            
+              axis: {range: [0, 9], tickwidth: 1, tickcolor: "darkblue" },            
+              bar: {color: 'rgb(49,130,189)', opacity:0.75},            
+              bgcolor: "white",            
+              borderwidth: 2,                        
+              steps: [            
+                {range: [0, 1], color: '#ffffd9'},
+                {range: [1,2], color: '#edf8b1'},
+                {range: [2,3], color: '#c7e9b4'},
+                {range: [3,4], color: '#7fcdbb'},
+                {range: [4,5], color: '#41b6c4'},
+                {range: [5,6], color: '#1d91c0'},
+                {range: [6,7], color: '#225ea8'},
+                {range: [7,8], color: '#0c2c84'},
+                {range: [8,9], color: '#011d6b'}                      
+              ],           
+              threshold: {            
+                line: {color: "red", width: 4},            
+                thickness: 0.75,            
+                value: number           
+              },          
+            }            
+          }
+    ];
+    let layout = {
+        width: 500,
+        height: 400,
+    };
+    Plotly.newPlot("gauge", washData, layout);
+}
+
+init();
